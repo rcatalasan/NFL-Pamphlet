bb451f6e9df0e5959c56455ac913923c40f04823
jamespjohnson <45546400+jamespjohnson@users.noreply.github.com>
2021-02-15 17:40:18 -0800

    Add files via upload

---
+++ readFile.h
@@ -0,0 +1,14 @@
+#ifndef READFILE_H
+#define READFILE_H
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+// ******************
+// readFile() - initializes link list to the given .csv file
+// TODO: Add linked list as a parameter
+void readFile();
+// ******************
+
+#endif // READFILE_H
