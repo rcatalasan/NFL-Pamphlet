5417aed6a0afdb479990603dd43bb17dfded822d
jamespjohnson <45546400+jamespjohnson@users.noreply.github.com>
2021-02-26 14:48:23 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,8 @@
 
 int main(int argc, char *argv[])
 {
-    //****************************
-    // Linked list of teams:
-    Node* teams = new Node();
-    //****************************
-    readFile(&teams);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    delete teams;
     return a.exec();
 }
