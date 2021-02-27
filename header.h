a0bb0c581644d82edd13c18fee8199bc7e89c2bd
jamespjohnson <45546400+jamespjohnson@users.noreply.github.com>
2021-02-26 18:28:46 -0800

    Update header.h

--- header.h
+++ header.h
@@ -1,14 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <iostream>
 #include <string>
 #include <iomanip>
 #include <fstream>
-#include "Team.h"
+#include "team.h"
 
 using namespace std;
 
 const int AR_SIZE = 32;
-
-
 
 void inputFn(Team arr[], int size);
 
@@ -29,3 +30,5 @@ void surfaceTypeSort(Team arr[], int size);
 void roofTypeSort(Team arr[], int size);
 
 void dateOpenedSort(Team arr[], int size);
+
+#endif // HEADER_H
