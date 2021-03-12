bd2b3a03a23391f7b4a9b2f7d415f15725dd2bc8
jamespjohnson <45546400+jamespjohnson@users.noreply.github.com>
2021-03-12 14:50:16 -0800

    Update and rename Functions.cpp to functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,44 @@
+#include "header.h"
+#include <algorithm>
+
+// Function to put thousands
+// separators in the given integer
+string thousandSeparator(int n)
+{
+    string ans = "";
+
+    // Convert the given integer
+    // to equivalent string
+    string num = to_string(n);
+
+    // Initialise count
+    int count = 0;
+
+    // Traverse the string in reverse
+    for (int i = num.size() - 1;
+        i >= 0; i--) {
+        count++;
+        ans.push_back(num[i]);
+
+        // If three characters
+        // are traversed
+        if (count == 3) {
+            ans.push_back(',');
+            count = 0;
+        }
+    }
+
+    // Reverse the string to get
+    // the desired output
+    std::reverse(ans.begin(), ans.end());
+
+    // If the given string is
+    // less than 1000
+    if (ans.size() % 4 == 0) {
+
+        // Remove ','
+        ans.erase(ans.begin());
+    }
+
+    return ans;
+}
