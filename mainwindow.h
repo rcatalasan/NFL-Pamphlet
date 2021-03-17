34c7c28e5b4962a76f5950d5b04ec57f312bb323
rcatalasan <78126440+rcatalasan@users.noreply.github.com>
2021-03-17 16:34:20 -0700

    Update mainwindow.h
    added doxygen comments to functions

--- mainwindow.h
+++ mainwindow.h
@@ -24,23 +24,50 @@ public:
     int totalSize;
 
 public slots:
+    /// <summary>
+    /// This function expands the teams' display tables when the new team is added.
+    /// </summary>
     void expandTables();
 
 private slots:
+    /// <summary>
+    /// This function opens up the 'CONTACT US' menu when the option is selected.
+    /// </summary>
     void on_actionContactUs_triggered();
 
+    /// <summary>
+    /// This function changes the display to LIGHT MODE when selected.
+    /// </summary>
     void on_actionLight_Mode_triggered();
 
+    /// <summary>
+    /// This function changes the display to DARK MODE when selected.
+    /// </summary>
     void on_actionDark_Mode_triggered();
-
+    
+    /// <summary>
+    /// This function displays all the teams when clicked on.
+    /// </summary>
     void on_radioAll_clicked();
 
+    /// <summary>
+    /// This function displays only the teams affiliated with the AMERICAN FOOTBALL CONFERENCE when clicked on.
+    /// </summary>
     void on_radioAFC_clicked();
 
+    /// <summary>
+    /// This function displays only the teams affiliated with the NATIONAL FOOTBALL CONFERENCE when clicked.
+    /// </summary>
     void on_radioNFL_clicked();
 
+    /// <summary>
+    /// This function opens the 'MAINTENANCE' menu when called upon.
+    /// </summary>
     void on_actionMaintenance_triggered();
 
+    /// <summary>
+    /// This function displays the 'HELP' menu when called upon.
+    /// </summary>
     void on_actionHelp_triggered();
 
 private:
